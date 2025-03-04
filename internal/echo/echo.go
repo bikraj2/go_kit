10639dc6031db5bffdc6956669064d1f41bf92a0
bikraj2 <bikrajshrestha2@gmail.com>
2025-03-04 23:54:49 +0530

    feat(echo):simple print and redirect '<'

--- internal/echo/echo.go
+++ internal/echo/echo.go
@@ -2,15 +2,20 @@ package echo
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	helper "go_kit.com/internal"
 )
 
 type Echo struct {
-	Args []string
+	Args     []string
+	Text     string
+	Redirect string
+	FileName string
 	EchoOptions
 }
+
 type EchoOptions struct {
 	NewLine         bool
 	EscapeCharacter bool
@@ -19,17 +24,60 @@ type EchoOptions struct {
 var echoValidOptions = []string{"n", "e"}
 
 func (echo *Echo) ProcessCommands(args []string) error {
-	fmt.Println(args)
+
 	err := echo.processFlags(args)
 	if err != nil {
 		return err
 	}
-	for _, arg := range args {
+
+	// Extract redirection and file name
+	textParts := []string{}
+	parsingFlags := true
+	for i, arg := range args {
 		if strings.HasPrefix(arg, "-") {
+			if !parsingFlags {
+				return fmt.Errorf("usage: echo [flag] <string> [redirection, file_name]")
+			}
 			continue
 		}
+		parsingFlags = false
+
+		if arg == ">" || arg == ">>" || arg == "<" || arg == "<<" {
+			if i+1 < len(args) {
+				echo.Redirect = arg
+				echo.FileName = args[i+1]
+			} else {
+				return fmt.Errorf("missing file name after redirection operator")
+			}
+			break
+		}
+
+		textParts = append(textParts, arg)
 	}
+	echo.Text = strings.Join(textParts, " ")
+	// if echo.FileName {
+	// 	if echo.Redirect != "" {
+	// 		if echo.Redirect == ">" {
+	// 		}
+	// 	}
+	// }
+	switch echo.Redirect {
+	case "":
+		if echo.NewLine {
+			fmt.Println(echo.Text)
+		} else {
+			fmt.Println(echo.Text)
+		}
+	case "<":
+		file, err := os.Create(echo.FileName)
+		if err != nil {
+			return err
+		}
+		file.Write([]byte(echo.Text))
+	}
+
 	return nil
+
 }
 
 func (echo *Echo) processFlags(args []string) error {
@@ -48,22 +96,20 @@ func (echo *Echo) processFlags(args []string) error {
 			err := echo.setOption(flag)
 			if err != nil {
 				return err
-
 			}
 		} else if start_flag_parse {
 			return nil
 		}
 	}
 	return nil
-
 }
+
 func (echoOpt *EchoOptions) setOption(opt string) error {
 	switch strings.ToLower(opt) {
 	case "n":
 		echoOpt.NewLine = true
 	case "e":
 		echoOpt.EscapeCharacter = true
-
 	default:
 		return fmt.Errorf("%v is not a valid flag", opt)
 	}
